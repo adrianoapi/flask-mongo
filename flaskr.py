837c6ca43dc13bb85de29084499b1ebffb23f1c5
Adriano API <adrianoapi@hotmail.com>
2018-07-17 12:35:17 -0300

    inicinado conexao com o mongodb

--- flaskr.py
+++ flaskr.py
@@ -1,7 +1,16 @@
 
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client   = MongoClient('localhost', 27017)
+database = client['flask_mongo'];
+
+collection = database.collection_names(include_system_collections = False)
+
+for col in collection:
+    print col
 
 @app.route('/')
 def testing():
