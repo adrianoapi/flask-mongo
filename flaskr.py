9db762e657c8c006b94dfe4f7eb7d492fe4d01d3
Adriano API <adrianoapi@hotmail.com>
2018-07-17 13:03:29 -0300

    Insert com PyMongo

--- flaskr.py
+++ flaskr.py
@@ -1,20 +1,30 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client   = MongoClient('localhost', 27017)
-database = client['flask_mongo'];
-
-collection = database.collection_names(include_system_collections = False)
-
-for col in collection:
-    print col
+client     = MongoClient('localhost', 27017)
+database   = client['flask_mongo'];
+collection = database.books
 
 @app.route('/')
-def testing():
+def index():
     return render_template('main/home.html')
+
+@app.route('/books/new', methods = ['GET'])
+def new():
+    return render_template('books/new.html')
+
+@app.route('/books/create', methods = ['POST'])
+def create():
+    collection.insert_one({
+        'name'    : request.form['name'    ],
+        'subtitle': request.form['subtitle'],
+        'isbn'    : request.form['isbn'    ]
+    })
+    
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
